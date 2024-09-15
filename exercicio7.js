ce607ca5cbebf1a518633156ea76b0d2eead18db
DaiRodri <daianarodri@gmail.com>
2024-09-14 23:19:26 -0300

    Add files via upload

---
+++ exercicio7.js
@@ -0,0 +1,94 @@
+var x = 1
+var y = 1
+
+
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 2
+var y = 1
+
+
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 3
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 4
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 5
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 6
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 7
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 8
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 9
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
+var x = 10
+var y = 1
+while (y <= 10){
+
+mult = x * y
+console.log( + x +"x" + y + "=" + mult)
+y++
+
+}
