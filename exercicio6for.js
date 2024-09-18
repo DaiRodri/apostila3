ae939dfdd6b1f41c5e17b6ac436d93a0b2d3db30
DaiRodri <daianarodri@gmail.com>
2024-09-17 21:11:41 -0300

    Add files via upload

---
+++ exercicio6for.js
@@ -0,0 +1,12 @@
+//crie um programa que some de 1 a 1000 utlizando o for.
+
+
+let x = 1000
+let soma =0
+let cont
+for (cont=1; cont <= 1000; cont++ ){
+
+    soma = cont + soma
+
+    console.log(" o resultado é :" + soma)
+}
